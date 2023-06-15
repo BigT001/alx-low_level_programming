a9fd749e14350df2f738ef839539ff06156ede2e
Samuel_Stanley <sta99175@gmail.com>
2023-06-15 15:51:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a command using the put function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
