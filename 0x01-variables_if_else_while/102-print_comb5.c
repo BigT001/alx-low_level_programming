a0a395223688e2fa4420289f3799e60ee4bf6d00
Samuel_Stanley <sta99175@gmail.com>
2023-06-19 22:11:12 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
 
 /**
  *main - Prints all possible combination of two two-digit numbers.
- *Reture: Always 0.
+ *Return: Always 0.
  */
 
 int main(void)
@@ -29,7 +29,7 @@ int main(void)
 		}
 	}
 
-	putchar('n');
+	putchar('\n');
 
 	return (0);
 
