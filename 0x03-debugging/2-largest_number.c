d42fa67166d0a9fa1c7c90fbd60113fa7fd2a261
Samuel_Stanley <sta99175@gmail.com>
2023-06-21 16:55:04 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,4 @@
+carrie@ubuntu:/debugging$ cat 2-largest_number.c
 #include "main.h"
 
 /**
@@ -10,20 +11,20 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if (a > b && b > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }
