ee7420bb949529fdcb9f014a7b143709e8e90d0c
Samuel_Stanley <sta99175@gmail.com>
2023-07-03 12:38:36 -0700

    ./1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: memory where it's stored
+ * @src: memory where it is copied
+ * *@n: number of bytes
+ *
+ * Return: copied memory with n byted changed
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
+
