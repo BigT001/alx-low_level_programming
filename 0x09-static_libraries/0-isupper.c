1b003357ce1902470f0393a2004ab78c5def77b4
Samuel_Stanley <sta99175@gmail.com>
2023-07-07 13:53:34 -0700

    C - Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Uppercase returns 1 if not return 0
+ * @c: this is the char to check
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
