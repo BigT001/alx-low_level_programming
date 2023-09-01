3574f7fe98c00906ad4a38dfe1c84ff935536776
BigT001 <unclet1992@gmail.com>
2023-08-31 17:43:01 -0700

    main.c compile

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,7 +7,7 @@
  * Return: converted number
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int a;
 	unsigned int dec_val = 0;
@@ -17,7 +17,7 @@ unsigned int binary_to_unit(const char *b)
 
 	for (a = 0; b[a]; a++)
 	{
-		if (b[a] <'0' || b[a]> '1')
+		if (b[a] < '0' || b[a] > '1')
 			return (0);
 		dec_val = 2 * dec_val + (b[a] - '0');
 	}
