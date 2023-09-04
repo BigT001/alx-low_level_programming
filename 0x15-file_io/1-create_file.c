051b38bf18ee7e5b25e298fcdcfbc4508c56edb3
BigT001 <unclet1992@gmail.com>
2023-09-04 04:25:28 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,6 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-
 	of = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(of, text_content, len);
 
