de7170305257fe06b9609d20ddb94fd1cf383b46
BigT001 <unclet1992@gmail.com>
2023-09-28 12:46:50 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,18 +9,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int as;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-	return (dec_val);
-}
+	as = 0;
 
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+	if (b[len] & 1)
+	{
+		as += base_two;
+	}
+	}
+	return (as);
+}
